6562ade2f2f358b9ff9e9d8f3b91718639340631
Luiz <diegoo837ma@icloud.com>
2022-09-02 19:23:45 +0100

    Initial Commit

---
+++ dChat/dChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  dChatApp.swift
+//  dChat
+//
+//  Created by Luiz Andrade on 02/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct dChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
